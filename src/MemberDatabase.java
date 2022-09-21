a74ccff50e353e17de6f6f418620f87c977c0187
Abhijeet <abhijeets075@gmail.com>
2022-09-21 09:28:02 -0400

    find method and memberdatabase class

---
+++ src/MemberDatabase.java
@@ -0,0 +1,45 @@
+public class MemberDatabase {
+    private Member [] mlist;
+    private int size;
+
+    private int find(Member member)
+    {
+        for (int i = 0; i < mlist.length; i++)
+        {
+            if (mlist[i] == member)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private void grow()
+    {
+
+    }
+    public boolean add(Member member)
+    {
+
+    }
+    public boolean remove(Member member)
+    {
+
+    }
+    public void print ()
+    {
+
+    } //print the array contents as is
+    public void printByCounty()
+    {
+
+    } //sort by county and then zipcode
+    public void printByExpirationDate()
+    {
+
+    } //sort by the expiration date
+    public void printByName()
+    {
+
+    } //sort by last name and then first name
+}
