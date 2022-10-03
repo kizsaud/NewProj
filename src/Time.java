c3e4c6f416753598127809401b89eab5ac0a0e7f
abhijeets15 <114023972+abhijeets15@users.noreply.github.com>
2022-10-03 15:22:53 -0700

    Update Time.java

--- src/Time.java
+++ src/Time.java
@@ -1,4 +1,9 @@
-package ProjectOne;
+/**
+/Create a enumeration called time for 3 classes, PILATES, SPINNING, AND CARDIO, Each of these Enumerations
+ have a dateTime field which pertains to the time of each class.
+ @author Abhijeet Singh, Khizar Saud
+ */
+
 public enum Time {
     Pilates("9:30"),
     Spinning("14:00"),
@@ -6,10 +11,19 @@ public enum Time {
 
     private final String dateTime;
 
+    /**
+     This is a Time constructor which takes in time as a parameter and creates a time object.
+     @param dateTime The String which is going to be converted to a Time object.
+     */
     Time(String dateTime) {
         this.dateTime = dateTime;
     }
 
+
+    /**
+     This returns time of the current Time object.
+     @return String of the time
+     */
     public String getDateTime() {
         return dateTime;
     }
