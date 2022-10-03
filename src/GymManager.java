0fa931eeb491b27a4672fd72e23634dbd01f43c2
abhijeets15 <114023972+abhijeets15@users.noreply.github.com>
2022-10-03 15:21:29 -0700

    Update GymManager.java

--- src/GymManager.java
+++ src/GymManager.java
@@ -1,5 +1,13 @@
-package ProjectOne;
+/**
+ This is the Gym Manager which is where the program run function is.
+ There are helper methods for adding a member to database, and reading commands.
+ This class takes care of adding members, removing members, printing members, adding and dropping memeber from fitness
+ class and displaying the fitness Schedule.
+ @author Abhijeet Singh, Khizar Saud
+ */
+
 import java.util.Scanner;
+
 
 public class GymManager {
     MemberDatabase db = new MemberDatabase();
@@ -8,6 +16,12 @@ public class GymManager {
     int classes = 3;
     //FitnessClass[] fitnessClasses = new FitnessClass[classes];
 
+
+    /**
+     This is the main run class.
+     This makes and stores all the fitness classes.
+     This method also collects all the parts of a command and sends it out to an execute command class.
+     */
     public void run() {
         fitnessClasses[0] = new FitnessClass("Pilates", "Jennifer", Time.Pilates, db, fitnessClasses);
         fitnessClasses[1] = new FitnessClass("Spinning", "Denise", Time.Spinning, db, fitnessClasses);
@@ -25,8 +39,14 @@ public class GymManager {
         }
     }
 
+    /**
+     Deletes the person with the given name from the list.
+     Does nothing if name doesn't appear in the list.
+     @param command This is the command to take care of the first letter command.
+     @param commands This is to store the rest of the input if there is any, for inforamtion about member for add/remove
+     */
     public void executeCommands(String command, String[] commands) {
-        if ("A".equals(command)) {
+        if (command.equals("A")) {
             if (addMemberToDatabase(commands)) {
                 System.out.println("member added to database");
             }
@@ -34,28 +54,9 @@ public class GymManager {
                 System.out.println("Member already in database");
             }
         }
-        else if("C".equals(command)){
-            String fName = commands[2];
-            String lName = commands[3];
-            String className = commands[1];
-            Date dob = new Date(commands[4]);
-            Member findMember = new Member(fName,lName,dob);
 
-            if (db.contains(findMember) != -1) {
-                findMember = db.getMember(db.contains(findMember));
-                for (int i = 0; i < fitnessClasses.length; i++) {
-                    if ((fitnessClasses[i].getFitnessName()).equalsIgnoreCase(className)) {
-                        fitnessClasses[i].checkIn(findMember, className, fitnessClasses, db);
-                        return;
-                    }
-                }
-                System.out.println(className + " class does not exist");
-            } else {
-                System.out.println(fName + " " + lName + " " + dob.toString() + " is not in the database.");
-            }
-        }
 
-        else if ("R".equals(command)) {
+        else if (command.equals("R")) {
             Date date = new Date(commands[3]);
             Member m = new Member();
             m.setFname(commands[1]);
@@ -69,39 +70,90 @@ public class GymManager {
             }
 
         }
-        else if ("P".equals(command)) {
+        else if (command.equals("P")) {
             System.out.println("-list of members-");
             db.print();
             System.out.println("-end of list-");
             System.out.println();
         }
-        else if ("PC".equals(command)) {
+        else if (command.equals("PC")) {
             System.out.println("-list of members sorted by county and zipcode-");
             db.printByCounty();
             System.out.println("-end of list-");
             System.out.println();
         }
-        else if ("PN".equals(command)) {
+        else if (command.equals("PN")) {
             System.out.println("-list of members sorted by last name, and first name-");
             db.printByName();
             System.out.println("-end of list-");
             System.out.println();
         }
-        else if ("PD".equals(command)) {
+        else if (command.equals("PD")) {
             System.out.println("-list of members sorted by membership expiration date-");
             db.printByExpirationDate();
             System.out.println("-end of list-");
             System.out.println();
         }
-        else if ("Q".equals(command)) {
+        else if (command.equals("Q")) {
             System.out.println("Gym Manager terminated.");
             System.exit(0);
         }
-        else if("C".equals(command)){
+        else if(command.equals("C")){
+            String fName = commands[2];
+            String lName = commands[3];
+            String className = commands[1];
+            Date dob = new Date(commands[4]);
 
+            Member m = new Member();
+            m.setDob(dob);
+            m.setFname(fName);
+            m.setLname(lName);
+
+            if (db.contains(m) != -1) {
+                m = db.getMember(db.contains(m));
+                for (int i = 0; i < fitnessClasses.length; i++) {
+                    if ((fitnessClasses[i].getFitnessName()).equalsIgnoreCase(className)) {
+                        fitnessClasses[i].checkIn(m, className, fitnessClasses, db);
+                        return;
+                    }
+                }
+                System.out.println(className + " class does not exist");
+            } else {
+                System.out.println(fName + " " + lName + " " + dob.toString() + " is not in the database.");
+            }
+        }
+        else if(command.equals("D")){
+            String fitnessClassName = commands[1];
+            String firstName = commands[2];
+            String lastName = commands[3];
+            Date dob = new Date(commands[4]);
+            Member m = new Member();
+            m.setLname(lastName);
+            m.setFname(firstName);
+            m.setDob(dob);
+            for (int i = 0; i < fitnessClasses.length; i++) {
+                if (fitnessClasses[i].getFitnessName().equalsIgnoreCase(fitnessClassName)) {
+                    fitnessClasses[i].dropClass(new Member(firstName, lastName, dob), db);
+                    return;
+                }
+            }
+            System.out.println("class not exist");
+        }
+        else if(command.equals("S")){
+            System.out.println("-Fitness classes-");
+            for (int i = 0; i < fitnessClasses.length; i++) {
+                fitnessClasses[i].printSchedule();
+            }
         }
     }
 
+    /**
+     This is a helper method for adding a member to the member database.
+     It takes care of valid, date of birth, expiration, and location to properly add a member.
+     Also takes care of if there is already a member with the same information in the database.
+     @param commands Gives the list of commands for whatever comes after Add command with name, dates, and location.
+     @return true if the member was added, and false if the member wasn't added.
+     */
     public boolean addMemberToDatabase(String[] commands) {
         String location = commands[5];
         Date exp = new Date(commands[4]);
@@ -156,3 +208,4 @@ public class GymManager {
 
     }
 }
+   
