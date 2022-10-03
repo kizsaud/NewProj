83f28f4c51cde9992ee2fa7d37d655b23b3884ea
kizsaud <100095731+kizsaud@users.noreply.github.com>
2022-10-03 15:29:58 -0400

    Create Time.java

---
+++ src/Time.java
@@ -0,0 +1,17 @@
+package ProjectOne;
+public enum Time {
+    Pilates("9:30"),
+    Spinning("14:00"),
+    Cardio("14:00");
+
+    private final String dateTime;
+
+    Time(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+}
