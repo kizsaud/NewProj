503feaa5ec94f3f5ec55b86f6ee866d9c7217360
abhijeets15 <114023972+abhijeets15@users.noreply.github.com>
2022-10-03 07:47:17 -0700

    Update GymManager.java

--- src/GymManager.java
+++ src/GymManager.java
@@ -2,143 +2,126 @@ import java.util.Scanner;
 
 public class GymManager {
     MemberDatabase db = new MemberDatabase();
-    public GymManager() {
-        MemberDatabase db = new MemberDatabase();
-    }
+    int classes = 3;
+    FitnessClass[] fitnessClasses = new FitnessClass[classes];
 
-    public void run(){
+    public void run() {
+
         System.out.println("Gym Manager running...");
         Scanner scan = new Scanner(System.in);
+        String command = "";
+        String[] commands;
         String user = "";
         int ptr = 0;
-        while(scan.hasNext() || user.equals("Q")){
-            user = scan.nextLine();
-            String command = "";
-            if(user.contains(" ")){
-                command = user.substring(0,user.indexOf(" "));
-            }
-            else{
-                command = user.substring(0, user.length());
-            }
-            ptr = user.indexOf(" ", ptr) + 1;
+        while (scan.hasNext()) {
+            command = scan.nextLine();
+            commands = command.split("\\s");
+            executeCommands(commands[0], commands);
+        }
+    }
 
-            // ADD Member
-            if(command.equals("A")){
-                //decalre new nember
-                Member m = new Member();
-                // Find Member attributes from string
-                String firstName = user.substring(ptr, user.indexOf(" ", ptr));
-                ptr = user.indexOf(" ", ptr) + 1;
-                String lastName = user.substring(ptr, user.indexOf(" ", ptr));
-                ptr = user.indexOf(" ", ptr) + 1;
-                String dobDate = user.substring(ptr, user.indexOf(" ", ptr));
-                ptr = user.indexOf(" ", ptr) + 1;
-                String expDate = user.substring(ptr, user.indexOf(" ", ptr));
-                ptr = user.indexOf(" ", ptr) + 1;
-                String city = user.substring(ptr);
-                ptr = user.indexOf(" ", ptr) + 1;
-
-                //Declare a date and Location for the Member
-                Date exp = new Date(expDate);
-                Date dob = new Date(dobDate);
-
-                Date today = new Date();
-                //Set attributes into member
-                if(dob.compareTo(today) == 1 || dob.compareTo(today) == 0){
-                    System.out.println("cannot be today or a future date!");
-                    continue;
-                }
-                else if(dob.isAdult() == false){
-                    System.out.println("DOB " + dobDate + ": must be 18 or older to join!");
-                    continue;
-                }
-                else if(dob.isValid() == false){
-                    System.out.println("DOB " + dobDate + ": invalid calender date!");
-                    continue;
-                }
-                else if(exp.isValid() == false){
-                    System.out.println("Expiration date " + expDate + ": invalid calender date!");
-                    continue;
-                }
-                else{
-                    m.setFname(firstName);
-                    m.setLname(lastName);
-                    m.setDob(dob);
-                    m.setExpire(exp);
-                }
-                //set location of member
-                city = city.toUpperCase();
-                if(city.equals("BRIDGEWATER")){
-                    Location A = Location.Bridgewater;
-                    m.setLocation(A);
-                }
-                else if(city.equals("EDISON")){
-                    Location A = Location.Edison;
-                    m.setLocation(A);
-                }
-                else if(city.equals("FRANKLIN")){
-                    Location A = Location.Franklin;
-                    m.setLocation(A);
-                }
-                else if(city.equals("SOMERVILLE")){
-                    Location A = Location.Somerville;
-                    m.setLocation(A);
-                }
-                else if(city.equals("PISCATAWAY")){
-                    Location A = Location.Piscataway;
-                    m.setLocation(A);
-                }
-                else{
-                    System.out.println("invalid location!");
-                    continue;
-                }
-
-                //put member in the member database if they do not exist
-                if(db.add(m) == true){
-                    System.out.println(firstName + " " + lastName + " added.");
-                }
-                else{
-                    System.out.println(firstName + " " + lastName + " is already in the database.");
-                }
-            }
-
-
-            //REMOVE Member
-            else if(command.equals("R")){
-                Member m = new Member();
-                String firstName = user.substring(ptr, user.indexOf(" ", ptr));
-                ptr = user.indexOf(" ", ptr) + 1;
-                String lastName = user.substring(ptr, user.indexOf(" ", ptr));
-                ptr = user.indexOf(" ", ptr) + 1;
-                String dobDate = user.substring(ptr);
-                ptr = user.indexOf(" ", ptr) + 1;
-                Date dob = new Date(dobDate);
-                m.setFname(firstName);
-                m.setLname(lastName);
-                m.setDob(dob);
-                m.setDob(dob);
-
-                if(db.remove(m) == true){
-                    System.out.println(firstName + " " + lastName + "removed.");
-                }
-                else{
-                    System.out.println(firstName + " " + lastName + " is not in the database.");
-                }
-
-            }
-            else if(command.equals("P")){
-                db.print();
-            }
-            else if(command.equals("PC")){
-                db.printByCounty();
-            }
-            else if(command.equals("PN")){
-                db.printByName();
-            }
-            else if(command.equals("PD")){
-                db.printByExpirationDate();
+    public void executeCommands(String command, String[] commands) {
+        if ("A".equals(command)) {
+            if (addMemberToDatabase(commands)) {
+                System.out.println("member added to database");
             }
         }
+        else if ("R".equals(command)) {
+            Date date = new Date(commands[3]);
+            Member m = new Member();
+            m.setFname(commands[1]);
+            m.setLname(commands[2]);
+            m.setDob(date);
+            if(db.remove(m) == true){
+                System.out.println(commands[1] + " " +commands[2] + " removed.");
+            }
+            else{
+                System.out.println(commands[1] + " " + commands[2] + " doesn't exist.");
+            }
+
+        }
+        else if ("P".equals(command)) {
+            System.out.println("-list of members-");
+            db.print();
+            System.out.println("-end of list-");
+            System.out.println();
+        }
+        else if ("PC".equals(command)) {
+            System.out.println("-list of members sorted by county and zipcode-");
+            db.printByCounty();
+            System.out.println("-end of list-");
+            System.out.println();
+        }
+        else if ("PN".equals(command)) {
+            System.out.println("-list of members sorted by last name, and first name-");
+            db.printByName();
+            System.out.println("-end of list-");
+            System.out.println();
+        }
+        else if ("PD".equals(command)) {
+            System.out.println("-list of members sorted by membership expiration date-");
+            db.printByExpirationDate();
+            System.out.println("-end of list-");
+            System.out.println();
+        }
+        else if ("Q".equals(command)) {
+            System.out.println("Gym Manager terminated.");
+            System.exit(0);
+        }
+    }
+
+    public boolean addMemberToDatabase(String[] commands) {
+        String location = commands[5];
+        Date exp = new Date(commands[4]);
+        Date dob = new Date(commands[3]);
+        boolean checkLocation = true;
+        location = location.toUpperCase();
+        if (location.equals("EDISON")) {
+            checkLocation = true;
+        }
+        else if (location.equals("BRIDGEWATER")) {
+            checkLocation = true;
+        }
+        else if (location.equals("SOMERVILLE")) {
+            checkLocation = true;
+        }
+        else if (location.equals("PISCATAWAY")) {
+            checkLocation = true;
+        }
+        else if (location.equals("FRANKLIN")) {
+            checkLocation = true;
+        } else {
+            checkLocation = false;
+        }
+        if (dob.isValid() && dob.isAdult() && exp.isValid() && checkLocation == true) {
+            Member m = new Member();
+            m.setDob(dob);
+            m.setExpire(exp);
+            m.setFname(commands[1]);
+            m.setLname(commands[2]);
+
+            //Split location back to match enum location
+            String finalLocation1 = location.substring(1).toLowerCase();
+            String finalLocation2 = location.substring(0,1).toUpperCase();
+
+            //Put it back together here
+            location = finalLocation2 + finalLocation1;
+            m.setLocation(Location.valueOf(location));
+            return db.add(m);
+        } else {
+            if(dob.isAdult() == false){
+                System.out.println("DOB " + dob.getMonth() + "/" + dob.getDay() + "/" + dob.getYear() + ": must be 18 or older to join!");
+            }
+            else if (dob.isValid() == false) {
+                System.out.println("DOB " + dob.getMonth() + "/" + dob.getDay() + "/" + dob.getYear() + ": invalid calender date!");
+            } else if (exp.isValid() == false) {
+                System.out.println("Expiration date " + exp.getMonth() + "/" + exp.getDay() + "/" + exp.getYear()  + ": invalid calender date!");
+            } else if (checkLocation == false) {
+                return false;
+            }
+            return false;
+        }
+
     }
 }
    
