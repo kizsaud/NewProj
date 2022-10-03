c46567d6197a88da3ce570ae8b1534ec35909292
abhijeets15 <114023972+abhijeets15@users.noreply.github.com>
2022-10-03 15:22:06 -0700

    Update Date.java

--- src/Date.java
+++ src/Date.java
@@ -1,4 +1,10 @@
-import java.util.StringTokenizer;
+/**
+ This class is for the Dates that are being used throughout the program.
+ This class takes care of any invalid Dates and also checks to see if the member is an adult.
+ This class is able to make date objects that are comaprable to today's date to see if a date is valid.
+ Today's date also helps with seeing if the member is an adult.
+ @author Abhijeet Singh, Khizar Saud
+ */
 import java.util.Calendar;
 public class Date implements Comparable<Date>{
     private int year=0;
@@ -6,6 +12,11 @@ public class Date implements Comparable<Date>{
     private int day=0;
     //Create object with todays date.
 
+
+    /**
+     This is the Date constructor for creating a date object given the string for the date.
+     @param D the string to convert the string into type Date
+     */
     public Date(String D){
         String[] Values=D.split("/");
         boolean leap=false;
@@ -19,9 +30,11 @@ public class Date implements Comparable<Date>{
 
 
 
-    
 
 
+    /**
+     This is the Date constructor for creating a date object with the current date, having the elements of today's date.
+     */
     public Date(){
 
         Calendar cal = Calendar.getInstance();
@@ -34,26 +47,50 @@ public class Date implements Comparable<Date>{
 
     }
 
+    /**
+     This is a setter method for setting the year of a Date object.
+     @param year the year which is passed to set
+     */
     public void setYear(int year) {
         this.year = year;
     }
 
+    /**
+     This is a setter method for setting the month of a Date object.
+     @param month the month which is passed to set
+     */
     public void setMonth(int month) {
         this.month = month;
     }
 
+    /**
+     This is a setter method for setting the day of a Date object.
+     @param day the day which is passed to set
+     */
     public void setDay(int day) {
         this.day = day;
     }
 
+    /**
+     This is a getter method for getting the year of a Date object.
+     @return the year which the current Date object has.
+     */
     public int getYear() {
         return year;
     }
 
+    /**
+     This is a getter method for getting the month of a Date object.
+     @return the month which is set in the current Date object.
+     */
     public int getMonth() {
         return month;
     }
 
+    /**
+     This is a getter method for getting the month of a Date object.
+     @return the day which the current Date object is set at.
+     */
     public int getDay() {
 
         return day;
@@ -61,7 +98,11 @@ public class Date implements Comparable<Date>{
 
 
 
-
+    /**
+     This compares the passed date with the current this.date, but checking the year, then month, then day.
+     @param date the date of the current date object.
+     @return the value 0 if the years are same, 1 if this.year is greater, -1 if this.year is less
+     */
     @Override
     public int compareTo(Date date) {
         //This denotes the year you created, date.year denotes the current date
@@ -107,6 +148,11 @@ public class Date implements Comparable<Date>{
         }
         return Winner;
     }
+
+    /**
+     This is a method to return if the current date is a leap year.
+     @return true if the year is a leap year, false otherwise
+     */
     public boolean isLeap(){
         if (year % 4 == 0) {
 
@@ -130,6 +176,10 @@ public class Date implements Comparable<Date>{
             return false;
     }
 
+    /**
+     This method checks to see if the year that is passed is a leap year instead of checking this.date
+     @param year the year which is being checked.
+     */
 public boolean isLeap(int year){
         
         if (year % 4 == 0) {
@@ -153,6 +203,11 @@ public boolean isLeap(int year){
         else
             return false;
     }
+
+    /**
+     This returns if the current member, this.date is an adult by seeing if they are 18 or older.
+     @return true if the member is an adult, false otherwise.
+     */
     public boolean isAdult(){
         boolean isAdult = false;
         Date A = new Date();
@@ -162,7 +217,6 @@ public boolean isLeap(int year){
         int day2=this.getDay();
         if(this.getYear()==Minyear){
             if(month==this.getMonth() || this.getMonth()<month){
-                System.out.println("hi");
                 if(day2<=day){
                     isAdult=true;
                 }
@@ -181,6 +235,11 @@ public boolean isLeap(int year){
 
         return isAdult;
     }
+    /**
+     Return true if the current date of birth is an adult and false otherwise.
+     @param DOB The date of birth which is passed.
+     @return true if the DOB is adult date of birth, false otherwise.
+     */
     public boolean isAdult(Date DOB){
         boolean isAdult = false;
         Date A = new Date();
@@ -205,6 +264,12 @@ public boolean isLeap(int year){
 
             return isAdult;
         }
+
+    /**
+     This is a method for checking if the date is an adult, having a String passed foe date.
+     @param Date The date which is passed
+     @return true if the date passed is of adult age, false otherwise.
+     */
     public boolean isAdult(String Date){
         String[] Values=Date.split("/");
         int checkYear=Integer.parseInt(Values[2]);
@@ -248,14 +313,25 @@ public boolean isLeap(int year){
 
 return isAdult;
             }
-        
-     
 
 
-    
 
+
+
+    /**
+     This is a method to check if the current date this.date is a date that exists.
+     @return true if the date is a valid date, false otherwise.
+     */
     public boolean isValid(){
-
+        if(month <= 0 || month > 12){
+            return false;
+        }
+        if(year <= 1900){
+            return false;
+        }
+        if(day <= 0){
+            return false;
+        }
         if ((month == 4 || month == 6 || month == 9 || month == 11) && day <= 30){
             return true;
         }
@@ -280,7 +356,13 @@ return isAdult;
 
             
         }
-        
+
+    /**
+     This method checks if the date is a valid date given multiple paramaeter of date.
+     @param month the month of the date which is passed.
+     @param day The day which is passed.
+     @param year The year which is passed.
+     */
     public boolean isValid(int month,int day , int year){
 
         if ((month == 4 || month == 6 || month == 9 || month == 11) && day <= 30){
